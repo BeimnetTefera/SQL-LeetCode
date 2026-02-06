139b57c04e5265e207c80ac50a33ec2b6e1e09f9
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-02-06 13:16:33 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (15.11%) - LeetSync

--- 500-keyboard-row/keyboard-row.py
+++ 500-keyboard-row/keyboard-row.py
@@ -5,34 +5,20 @@ class Solution:
         row1 = set("qwertyuiop")
         row2 = set("asdfghjkl")
         row3 = set("zxcvbnm")
-        
-        
+
         for word in words:
-            for i in range(3):
-                Yes = True
-                if i == 0:
-                    for char in word:
-                        if char.lower() not in row1:
-                            Yes = False
-                    if Yes:
-                        output.append(word)
-                elif i == 1:
-                    for char in word:
-                        if char.lower() not in row2:
-                            Yes = False
-                    if Yes:
-                        output.append(word)
-                elif i == 2:
-                    for char in word:
-                        if char.lower() not in row3:
-                            Yes = False
-                    if Yes:
-                        output.append(word)
-        print(output)
+            count_1 = 0
+            count_2 = 0
+            count_3 = 0
+            for char in word.lower():
+                if char in row1:
+                    count_1 += 1
+                elif char in row2:
+                    count_2 += 1
+                else:
+                    count_3 += 1
+            
+            if len(word) in [count_1, count_2, count_3]:
+                output.append(word)
+
         return output
-        
-        # p - number of words
-        # q - length of longest word
-        # 3 - no. of rows
-        
-        # Time complexity - O(3.p.q) - O(pq)
