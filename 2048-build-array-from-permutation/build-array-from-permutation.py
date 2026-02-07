a98feb2ed70cfe06ef2336d328c5c2dc5090c26a
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-02-07 08:44:11 +0300

    Time: 1 ms (62.05%) | Memory: 19.6 MB (17.00%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,20 +1,8 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        result = []
-        for i in range(len(nums)):
-            result.append(nums[nums[i]])
-        return result
-        
-"""
-nums = 0, 2, 1, 5, 3, 4
-idx =. 0, 1, 2, 3, 4, 5
+        answer = []
 
-nums[nums[0]] =nums[0] = 0
-nums[nums[1]] =nums[2] = 1
-nums[nums[2]] =nums[1] = 2
-nums[nums[3]] =nums[5] = 4
-nums[nums[4]] =nums[3] = 5
-nums[nums[5]] =nums[4] = 3
+        for num in nums:
+            answer.append(nums[num])
 
-0,1,2,4,5,3
-"""
+        return answer
